b32c19360ddd093eba7f89b30e5898771bc7915e
bulbrain <georgi_d_g1@abv.bg>
2015-05-13 00:47:34 +0300

    added High-Quality-Classes

---
+++ 8. High-Quality-Classes/Cohesion-and-Coupling/Calculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CohesionAndCoupling
+{
+    static class Calculator
+    {
+        public static double Width { get; set; }
+        public static double Height { get; set; }
+        public static double Depth { get; set; }
+
+        
+
+        public static double CalcDistance2D(double x1, double y1, double x2, double y2)
+        {
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            return distance;
+        }
+
+        public static double CalcDistance3D(double x1, double y1, double z1, double x2, double y2, double z2)
+        {
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+            return distance;
+        }
+
+        public static double CalcVolume()
+        {
+            double volume = Width * Height * Depth;
+            return volume;
+        }
+
+        public static double CalcDiagonalXYZ()
+        {
+            double distance = Math.Sqrt((Width * Width) + (Height * Height) + (Depth * Depth));
+            return distance;
+        }
+
+        public static double CalcDiagonalXY()
+        {
+            double distance = Math.Sqrt((Width * Width) + (Height * Height));
+            return distance;
+        }
+
+        public static double CalcDiagonalXZ()
+        {
+            double distance = Math.Sqrt((Width * Width) + (Depth * Depth));
+            return distance;
+        }
+
+        public static double CalcDiagonalYZ()
+        {
+            double distance = Math.Sqrt((Height * Height) + (Depth * Depth));
+            return distance;
+        }
+    }
+}
